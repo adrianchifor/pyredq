cec8cbadb4faa70cd99155789c640a133b3b7a6f
Adrian Chifor <me@adrianchifor.com>
2020-06-03 18:05:22 +0100

    Version increment 0.2.1

--- pyredq/version.py
+++ pyredq/version.py
@@ -1,7 +1,7 @@
 title = 'pyredq'
 description = 'Light and fast ordered message queue for Python3 using Redis'
 url = 'https://github.com/adrianchifor/pyredq'
-version = '0.2.0'
+version = '0.2.1'
 author = 'Adrian Chifor'
 author_email = 'me@adrianchifor.com'
 license = 'Apache License 2.0'
